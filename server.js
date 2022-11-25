aa70ca3ae66aa8e4f0868499c41b91aa5f05fe9c
superdev <superdev0241002@gmail.com>
2022-11-26 02:26:22 +0900

    A feat: Start server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+})
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
