26f678f80641558b2756b0a0550cf75c72bd3fd3
superdev <superdev0241002@gmail.com>
2022-12-01 05:21:03 +0900

    A fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,8 @@
 const User = require("../model/user");
 const jwt = require("jsonwebtoken");
-// @ get Random string
-function makeid(length) {
-  var result = "";
-  var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
+
 exports.register = async (req, res) => {
   try {
-
     const { first_name, last_name, email, password } = req.body;
     // Validate user input
     if (!(email && password && first_name && last_name))
@@ -37,15 +26,47 @@ exports.register = async (req, res) => {
         user_id: user._id,
         email,
       },
-      makeid(Math.random() % 10),
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
       { expiresIn: "2h" }
     );
-    // save user token
+    // // save user token
     user.token = token;
     // return new user
-    res.status(201).json(user);
+    res.status(200).send({msg:"success"});
   } catch (err) {
     console.log(err);
   }
   /**bcrypt password is temporary suspend */
+};
+
+exports.login = async (req, res) => {
+  try {
+    // Get user data
+    const { email, password } = req.body;
+    // Validate your input
+    if (!(email && password)) res.status(400).send("All input is required");
+    //Validate if the user exist in database
+    const user = await User.findOne({ email });
+
+    if (user && (await (password === user.password))) {
+      const token = jwt.sign(
+        {
+          user_id: user._id,
+          email,
+        },
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+        { expiresIn: "2h" }
+      );
+      // save user token
+      user.token = token;
+
+      res.status(200).send({
+        msg : "success"
+      });
+    } else {
+      res.status(400).send("Invalid Credentials");
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,10 @@ module.exports = (app) => {
   const tutorials = require("../controllers/tutorialController.js");
   const user = require("../controllers/userController");
   let router = require("express").Router();
-
+  // User Router
   router.post("/register",user.register);
-
-  // Control Tutorial
+  router.post("/login", user.login);
+  // Tutorial Router
    router.get("/", tutorials.findAllorTitle);
    router.post("/", tutorials.create);
    router.delete("/", tutorials.deleteAll);
